e49ddaec5553221754d22eb4b92dd5beffd7cc87
Kristofer Doman <kristoferdoman@gmail.com>
2017-06-27 11:18:05 -0500

    10.) Layouts and Structure

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+
+@section('content')
+    <h1>A place to show a post.</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@
 
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
+
+Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
